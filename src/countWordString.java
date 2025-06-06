5da15b68f23709265fc1958dde21c6293b1ace80
maheshthoratFTPL <148130075+maheshthoratFTPL@users.noreply.github.com>
2025-06-06 17:06:34 +0530

    code comleted

---
+++ src/countWordString.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+public class countWordString {
+    public static void main(String[] args) {
+
+
+        String str = "Java fulstack Selenium";
+
+        String[] strWords =  str.split(" ");
+
+        System.out.println(strWords.length);
+
+
+
+    }
+}
